44913219152a89752eeb324b5391c6cc009e190b
abdujalilovsunnatjon-dev <abdujalilovsunnatjon.dev@gmail.com>
2026-01-09 17:07:13 +0500

    api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,7 @@
+// Frontend/src/lib/api.ts
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
+  baseURL: import.meta.env.VITE_API_URL, // <- должен быть установлен в Vercel
+  withCredentials: true, // если бекенд использует куки/сессии
 });
